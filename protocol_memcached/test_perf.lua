d8f107e952312a23d624f84652c64ca901c249ea
Steve Yen <steve.yen@gmail.com>
2011-09-13 09:50:16 -0700

    Better perf test

--- protocol_memcached/test_perf.lua
+++ protocol_memcached/test_perf.lua
@@ -9,6 +9,7 @@ client = memcached_client_ascii
 ------------------------------------------
 
 location = arg[1] or '127.0.0.1:11211'
+times    = tonumber(arg[2]) or 50000
 
 host, port, c = connect(location)
 c:setoption('tcp-nodelay', true)
@@ -16,28 +17,27 @@ c:settimeout(nil)
 
 ------------------------------------------
 
-p("connected", host, port, c)
+p("connected:", host, port, c)
 
-local gotten = 0
-
+gotten = 0
 function got(...)
   gotten = gotten + 1
 end
 
-local request = {keys = {"a"}}
+request = {keys = {"a"}}
 
-local i = 0
-local times = 50000
+t_start = os.clock()
 
-local t_start = os.clock()
-
+i = 0
 while i < times do
   assert(client.get(c, got, request) == "END")
   i = i + 1
 end
 
-local t_end = os.clock()
+t_end = os.clock()
 
-p("ops/sec: ", times / (t_end - t_start))
+p("requests:     ", times)
+p("replies:      ", gotten)
+p("secs/request: ", (t_end - t_start) / times)
+p("requests/sec: ", times / (t_end - t_start))
 
-p("done", i, gotten)
