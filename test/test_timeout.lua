d2841bbb25990bafe2ed56023a3f517ab5532b58
Steve Yen <steve.yen@gmail.com>
2011-10-01 12:23:08 -0700

    More timeout asserts.

--- test/test_timeout.lua
+++ test/test_timeout.lua
@@ -20,23 +20,35 @@ end
 a1_addr = ambox.spawn(a1, 2)
 
 pstats("pre-cycle")
+assert(ambox.stats().cur_timeout == 1)
+assert(ambox.stats().tot_timeout == 0)
+
 t = ambox.cycle()
 assert(t == 2)
+assert(ambox.stats().cur_timeout == 1)
+assert(ambox.stats().tot_timeout == 0)
 
 pstats("pre-sleep")
 os.execute('sleep 3')
 
 t = ambox.cycle()
 assert(t == 2)
+assert(ambox.stats().cur_timeout == 1)
+assert(ambox.stats().tot_timeout == 1)
 
 pstats("pre-spawn")
 a2_addr = ambox.spawn(a1, 2)
 
 pstats("pre-sleep-more")
+assert(ambox.stats().cur_timeout == 2)
+assert(ambox.stats().tot_timeout == 1)
+
 os.execute('sleep 3')
 
 t = ambox.cycle()
 assert(t == 2)
+assert(ambox.stats().cur_timeout == 2)
+assert(ambox.stats().tot_timeout == 3)
 
 pstats("done")
 p("DONE")
