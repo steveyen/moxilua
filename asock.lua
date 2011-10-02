1f4efbbe2e0bb806091eb3605e2250cdf16afb1d
Steve Yen <steve.yen@gmail.com>
2011-10-01 20:02:22 -0700

    Update asock to ambox.recv() API

--- asock.lua
+++ asock.lua
@@ -111,7 +111,7 @@ local function recv(actor_addr, skt, pattern, part, partial_ok)
 
     skt_wait(skt, reading, reverse_r, actor_addr)
 
-    local s, skt_recv = ambox.recv(filter_skt, actor_addr)
+    local s, skt_recv = ambox.recv(filter_skt)
     assert(skt == skt_recv)
   until false
 end
@@ -132,7 +132,7 @@ local function send(actor_addr, skt, data, from, to)
 
     skt_wait(skt, writing, reverse_w, actor_addr)
 
-    local s, skt_recv = ambox.recv(filter_skt, actor_addr)
+    local s, skt_recv = ambox.recv(filter_skt)
     assert(skt == skt_recv)
   until false
 end
@@ -151,7 +151,7 @@ local function loop_accept(actor_addr, skt, handler, timeout)
 
     skt_wait(skt, reading, reverse_r, actor_addr)
 
-    local s, skt_recv = ambox.recv(filter_skt, actor_addr)
+    local s, skt_recv = ambox.recv(filter_skt)
     assert(skt == skt_recv)
   until false
 end
