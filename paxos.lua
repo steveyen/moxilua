b2d5e8ae3e8f7b8401fe6335e0d0f279b26a1444
Steve Yen <steve.yen@gmail.com>
2011-10-16 22:46:45 -0700

    More paxos

--- paxos.lua
+++ paxos.lua
@@ -1,37 +1,21 @@
-function prepare(seq, sender)
-  assert(seq >= 0)
-  assert(sender >= 0)
-  return { kind = 'prepare',
-           proposal_seq = { seq, sender } }
-end
+function paxos_module(ambox, opts)
 
-function prepared(proposal_seq)
-  assert(#proposal_seq == 2)
-  return { kind = 'prepared',
-           proposal_seq = proposal_seq }
-end
+ambox = ambox or require('ambox')
+opts  = opts  or {}
 
-function accept(seq, sender, value)
-  assert(seq >= 0)
-  assert(sender >= 0)
-  return { kind = 'accept',
-           proposal_seq = { seq, sender },
-           value = value }
-end
+local self_addr = ambox.self_addr
+local send = ambox.send
+local recv = ambox.recv
+local log = opts.log or print
 
-function accepted(proposal_seq)
-  assert(#proposal_seq == 2)
-  return { kind = 'accepted',
-           proposal_seq = proposal_seq)
-end
+local RES_NACK     = 1
+local REQ_PREPARE  = 10
+local RES_PREPARED = 11
+local REQ_ACCEPT   = 20
+local RES_ACCEPTED = 21
 
-function proposal_seq_gte(a, b)
-  a = a or { 0, -1 }
-  b = b or { 0, -1 }
-  a1 = a[1] or 0
-  b1 = b[1] or 0
-  return (a1 > b1) or (a1 == b1 and (a[2] or -1) >= (b[2] or -1))
-end
+local acceptor_timeout = opts.acceptor_timeout or 3
+local proposer_timeout = opts.proposer_timeout or 3
 
 function arr_member(arr, item)
   for i = 1, #arr do
@@ -42,67 +26,140 @@ function arr_member(arr, item)
   return false
 end
 
-function acceptor(current_proposal_seq,
-                  current_value,
-                  save_seq,
-                  save_seq_value)
-  local me = self_addr()
+function seq_gte(a, b) -- Returns true if a >= b.
+  a = a or { 0, -1 }   -- A seq is { seq_id, source_id }.
+  b = b or { 0, -1 }
+  a1 = a[1] or 0
+  b1 = b[1] or 0
+  return (a1 > b1) or (a1 == b1 and (a[2] or -1) >= (b[2] or -1))
+end
+
+function accept(storage, initial_state)
+  initial_state = initial_state or {}
+
+  local me           = initial_state.id or self_addr()
+  local accepted_seq = initial_state.accepted_seq
+  local accepted_val = initial_state.accepted_val
+  local proposal_seq = accepted_seq
+
+  function respond(to, kind, msg)
+    msg = msg or {}
+    msg.kind = kind
+    msg.accepted_seq = accepted_seq
+    msg.accepted_val = accepted_val
+    send(to, me, msg)
+  end
+
+  function process(source, req, kind, storage_fun)
+    if seq_gte(req.seq, proposal_seq) then
+      local ok, err = storage_fun(req.seq, req.val)
+      if ok then
+        respond(source, kind,
+                { req: { kind: req.kind, seq: req.seq } })
+        return true
+      else
+        respond(source, RES_NACK,
+                { req: req,
+                  err: { "storage error", err } })
+      end
+    else
+      respond(source, RES_NACK,
+              { req: req,
+                err: "req seq was behind" })
+    end
+    return false
+  end
 
   while true do
-    local msg = recv()
-    if msg and
-       msg.proposal_seq then
-      local source = msg.proposal_seq[2]
-      if source and
-         proposal_seq_gte(msg.proposal_seq, current_proposal_seq) then
-        if msg.kind == 'prepare' and
-           save_seq(msg.proposal_seq) then
-          current_proposal_seq = msg.proposal_seq
-          send(source, me, prepared(current_proposal_seq))
+    local req = recv(nil, acceptor_timeout)
+    if (req == 'die' or
+        req == 'timeout') then
+      return true, req, { id = me,
+                          accepted_seq = accepted_seq,
+                          accepted_val = accepted_val }
+    end
+
+    if req and req.seq then
+      local source = req.seq[2]
+      if source then
+        if req.kind == REQ_PREPARE then
+          if process(source, req, RES_PREPARED, storage.save_seq) then
+            proposal_seq = req.seq
+          end
+        elseif req.kind == REQ_ACCEPT then
+          if process(source, req, RES_ACCEPTED, storage.save_seq_val) then
+            proposal_seq = req.seq
+            accepted_seq = req.seq
+            accepted_val = req.val
+          end
+        else
+          log("paxos.accept", "unknown req.kind", req.kind)
         end
-        if msg.kind == 'accept' and
-           save_seq_value(msg.proposal_seq, msg.value) then
-          current_proposal_seq = msg.proposal_seq
-          current_value = msg.value
-          send(source, me, accepted(current_proposal_seq))
-        end
+      else
+        log("paxos.accept", "missing req.seq.source", req.seq)
       end
+    else
+      log("paxos.accept", "bad req")
     end
   end
 end
 
-function phase(seq, id, acceptors, msg, vote_kind)
-  for i, acceptor_addr in ipairs(acceptors) do
-    assert(acceptor_addr != id)
-    send(acceptor_addr, msg)
-  end
-
-  local quorum = math.floor(#acceptors / 2) + 1
-  local votes = {}
-
-  while true do
-    local source, msg = recv()
-    if arr_member(acceptors, source) and
-       msg.kind == vote_kind then
-      if msg.proposal_seq[1] == seq and
-         msg.proposal_seq[2] == id and
-         not arr_member(votes, source) then
-        votes[#votes] = source
-        if #votes >= quorum then
-          return
-        end
-      end
-    end
-  end
-end
-
--- Runs a single round of paxos to decide a value.
-function proposer(seq, id, acceptors, proposal_value)
+function propose(key, seq, id, acceptors, val)
   assert(#acceptors > 0)
   assert(seq >= 0)
   assert(id >= 0)
 
-  phase(seq, id, acceptors, prepare(seq, id), 'prepared')
-  phase(seq, id, acceptors, accept(seq, id, proposal_value), 'accepted')
+  function phase(req, yea_vote_kind)
+    for i, acceptor_addr in ipairs(acceptors) do
+      send(acceptor_addr, req)
+    end
+
+    local quorum = math.floor(#acceptors / 2) + 1
+    local tally = {}
+    tally[yea_vote_kind] = { {}, true, nil }
+    tally[RES_NACK]      = { {}, false, "rejected" }
+
+    while true do
+      local source, res = recv(nil, proposer_timeout)
+      if (source == 'die' or
+          source == 'timeout') then
+        return false, source
+      end
+
+      assert(arr_member(acceptors, source))
+      assert(res and res.req and res.req.seq)
+      assert(res.req.seq[1] == seq)
+      assert(res.req.seq[2] == id)
+      assert(tally[res.kind])
+
+      local tkind = tally[res.kind]
+      local votes = tkind[1]
+      if not arr_member(votes, source) then
+        votes[#votes] = source
+        if #votes >= quorum then
+          return tkind[2], tkind[3]
+        end
+      else
+        log("paxos.propose", "repeat vote from source", source, res)
+      end
+    end
+  end
+
+  local ok, err = phase({ kind = REQ_PREPARE,
+                          seq = { seq, id } }, REQ_PREPARED)
+  if not ok then return ok, err end
+
+  local ok, err = phase({ kind = REQ_ACCEPT,
+                          seq = { seq, id },
+                          val = val }, REQ_ACCEPTED)
+  if not ok then return ok, err end
+
+  return true
 end
 
+return { accept  = accept,
+         propose = propose }
+
+end
+
+return paxos_module()
---
+++ protocol_memcached/proxy_pipelined.lua
@@ -0,0 +1,157 @@
+-- The 'gval' server just has one global value. All GET's and SET's,
+-- no matter the key, operate on that single value. This is primarily
+-- meant for benchmarking ambox/asock layers, so we avoid testing
+-- lua's GC and memory handling capabilities.
+--
+-- We even store the gval optimized for GET requests.
+--
+local gval = "END\r\n"
+
+----------------------------------------
+
+socket = require('socket')
+
+local sbyte = string.byte
+local sfind = string.gfind
+
+ambox = require('ambox')
+asock = require('asock')
+
+require('util')
+
+require('protocol_memcached/client')
+require('protocol_memcached/server')
+
+-- trace_table(ambox, "ambox", { self_addr = 0 })
+-- trace_table(asock, "asock")
+
+local aself  = ambox.self_addr
+local acycle = ambox.cycle
+local astats = ambox.stats
+local srecv  = asock.recv
+local ssend  = asock.send
+local sstep  = asock.step
+local sstats = asock.stats
+
+function sock_recv(skt, pattern, part, partial_ok)
+  return srecv(aself(), skt, pattern, part, partial_ok)
+end
+
+function sock_send(skt, data, from, to)
+  return ssend(aself(), skt, data, from, to)
+end
+
+---------------------------------------------------
+
+memcached_server_ascii_gval = {
+  get = function(_, skt, cmd, arr)
+          if #arr == 1 then
+            return sock_send(skt, gval)
+          end
+          return sock_send(skt, "END\r\n")
+        end,
+
+  set = function(_, skt, cmd, arr)
+          local key, flag, expire, size = unpack(arr)
+          if key and flag and expire and size then
+            nsize = tonumber(size)
+            if nsize and nsize >= 0 then
+              local data, err = sock_recv(skt, nsize + 2) -- 2 for CR/NL.
+              if not data then
+                return data, err
+              end
+
+              gval = "VALUE " .. key .. " " .. flag .. " " .. size .. "\r\n" ..
+                     data .. "END\r\n"
+
+              return sock_send(skt, "STORED\r\n")
+            end
+          end
+
+          return sock_send(skt, "ERROR\r\n")
+        end
+}
+
+----------------------------------------
+
+function upstream_session_memcached_ascii_gval(env, upstream_skt)
+  local self_addr = ambox.self_addr()
+
+  local req = true
+  while req do
+    req = srecv(self_addr, upstream_skt)
+    if req then
+      if sbyte(req, 1) == 103 then -- 'g' assuming a 'get'
+        ssend(self_addr, upstream_skt, gval)
+      else
+        -- Using util/split() seems slightly slower than string.gfind()
+        -- on simplistic tests.
+        --
+        local itr = sfind(req, "%S+")
+        local cmd = itr()
+        if cmd and string.len(cmd) > 1 then
+          local spec = env.specs[cmd]
+          if spec then
+            if not spec(env.data, upstream_skt, cmd, iter_array(itr)) then
+              req = nil
+            end
+          else
+            ssend(self_addr, upstream_skt, "ERROR\r\n")
+          end
+        end
+      end
+    end
+  end
+
+  upstream_skt:close()
+end
+
+----------------------------------------
+
+local UPSTREAM_SESSION = "US"
+
+function upstream_accept(server_skt, sess_actor, env)
+  local acceptor_addr = ambox.self_addr()
+
+  local session_handler = function(upstream_skt)
+    upstream_skt:setoption('tcp-nodelay', true)
+    upstream_skt:settimeout(0)
+
+    ambox.spawn_kind(sess_actor, UPSTREAM_SESSIONS, env, upstream_skt)
+  end
+
+  asock.loop_accept(acceptor_addr, server_skt, session_handler)
+end
+
+------------------------------------------------------------
+
+local host, port = "127.0.0.1", 11322
+
+ambox.spawn(upstream_accept, socket.bind(host, port),
+            upstream_session_memcached_ascii_gval, {
+              specs = memcached_server_ascii_gval,
+              data = {}
+            })
+
+------------------------------------------------------------
+
+print("loop")
+
+local d = true
+local i = 0
+
+while true do
+  acycle(true)
+  sstep()
+
+  if d and (i % 20000 == 0) then
+    for k, v in pairs(astats()) do print(k, v) end
+    for k, v in pairs(sstats()) do print(k, v) end
+    print("")
+  end
+
+  i = i + 1
+end
+
+print("done")
+
